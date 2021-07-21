f6fe1cf30568599a4dee9aed303be50c036c9b5f
Reagan Sasan <reagansasani@hotmail.com>
2021-07-21 10:04:31 +0200

    new style

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+const video = document.getElementById("myVideo");
+
+// Get the button
+const btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
