4171db00ff4c2e6c79b00540baed7579c20dfeb8
Reagan Sasan <reagansasani@hotmail.com>
2021-07-25 07:20:08 +0200

    body completed + fixes

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const video = document.getElementById("videoBackground");
-
-// Get the button
 const btn = document.getElementById("myBtn");
 
-// Pause and play the video, and change the button text
 function myFunction() {
   if (video.paused) {
     video.play();
@@ -13,3 +10,54 @@ function myFunction() {
     btn.innerHTML = "Play";
   }
 }
+
+const initialVid = "./videos/video.mp4";
+const grillVid = "./videos/grillmix.mp4";
+const steakVid = "./videos/steak1.mp4";
+const burgerVid = "./videos/burger2.mp4";
+const steak = document.getElementById("steak1");
+const burger = document.getElementById("burger1");
+const grill = document.getElementById("grill1");
+
+steak.addEventListener(
+  "mouseover",
+  () => {
+    video.setAttribute("src", steakVid);
+  },
+  steak.addEventListener("mouseout", () => {
+    video.setAttribute("src", initialVid);
+  })
+);
+
+burger.addEventListener(
+  "mouseover",
+  () => {
+    video.setAttribute("src", burgerVid);
+  },
+  burger.addEventListener("mouseout", () => {
+    video.setAttribute("src", initialVid);
+  })
+);
+
+grill.addEventListener(
+  "mouseover",
+  () => {
+    video.setAttribute("src", grillVid);
+  },
+  grill.addEventListener("mouseout", () => {
+    video.setAttribute("src", initialVid);
+  })
+);
+
+window.onscroll = () => {
+  scrollRotate();
+};
+
+scrollRotate = () => {
+  let image = document.getElementById("reload");
+  let image2 = document.getElementById("reload2");
+  let image3 = document.getElementById("reload3");
+  image.style.transform = "rotate(" + window.pageYOffset / 10 + "deg)";
+  image2.style.transform = "rotate(" + window.pageYOffset / 10 + "deg)";
+  image3.style.transform = "rotate(" + window.pageYOffset / 10 + "deg)";
+};
